8e5a8ee056dcf9e6126f56ae4416da5ae2233ce6
RedEtherbloom <etherbloom@mailbox.org>
2025-10-02 15:14:08 +0200

    feat: rudimentary test endpoint

--- cmd/piper-tts-streaming/piper-tts-streaming.go
+++ cmd/piper-tts-streaming/piper-tts-streaming.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type TTSRequest struct {
+	SpeakerID string `json:"speaker_id"`
+	Text      string `json:"text"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
 func main() {
+	// TODO: Create config object
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	logger.Info("Hello World")
 
@@ -18,6 +31,25 @@ func main() {
 	router.Use(middleware.Logger)
 
 	router.Get("/", func(wri http.ResponseWriter, req *http.Request) {
-    wri.Write([]byte("Hello World"))
+		wri.Write([]byte("Hello World"))
 	})
+	router.Post("/tts/synthesize", func(wri http.ResponseWriter, req *http.Request) {
+		var tts TTSRequest
+		if err := json.NewDecoder(req.Body).Decode(&tts); err != nil {
+			wri.WriteHeader(http.StatusBadRequest)
+			error := fmt.Sprintf("Invalid TTS request: %v", err)
+			if err := json.NewEncoder(wri).Encode(ErrorResponse{Error: error}); err != nil {
+				logger.Error("Failed to Marshal error: %v", err)
+			}
+			return;
+		}
+
+		if strings.Contains(strings.ToLower(tts.Text), "you") {
+			wri.Write([]byte("You too :3"))
+		} else {
+			wri.Write([]byte("Why not?"))
+		}
+	})
+
+	http.ListenAndServe(":9000", router)
 }
